1b60a823daa0bf6b2297d9d1f16b95c7e2e3eb2c
wuchangping <wu.changping@iwhalecloud.com>
2024-01-31 16:58:19 +0800

    v1.0.7更新

--- packages/index.js
+++ packages/index.js
@@ -1,2 +1,5 @@
 import vueTranslateDirective from './vue-translate-directive'
+import { translateKey } from './vue-translate-directive'
+
 export default vueTranslateDirective
+export { translateKey }
